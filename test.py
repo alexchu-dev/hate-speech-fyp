1629f56213955c3b8f9e054b0e463e1f087f0d4d
Alex Chu <alex.chu@moderndentallab.com>
2024-12-31 15:33:48 +0800

    nth

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
-import os
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-from transformers import pipeline
-
-classifier = pipeline("sentiment-analysis")
-print(classifier("I love you"))
+import torch
+import pandas as pd
+import numpy as np
+from transformers import BertTokenizer, BertForSequenceClassification
+if __name__ == '__main__':
+    df = pd.read_csv('https://gist.githubusercontent.com/Mukilan-Krishnakumar/e998ecf27d11b84fe6225db11c239bc6/raw/74dbac2b992235e555df9a0a4e4d7271680e7e45/imdb_movie_reviews.csv')
+    df = df.drop('sentiment',axis=1)
+    tokenizer = BertTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+    model = BertForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+    def sentiment_movie_score(movie_review):
+        token = tokenizer.encode(movie_review, return_tensors = 'pt')
+        result = model(token)
+        return int(torch.argmax(result.logits))+1
+    df['sentiment'] = df['text'].apply(lambda x: sentiment_movie_score(x[:512]))
+    print(df)
